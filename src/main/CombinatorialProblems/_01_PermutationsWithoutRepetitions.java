2e3b3eb852d9ef7592e3daa28fdf8eca8b310c94
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-07-07 14:20:58 +0300

    add

--- src/main/CombinatorialProblems/_01_PermutationsWithoutRepetitions.java
+++ src/main/CombinatorialProblems/_01_PermutationsWithoutRepetitions.java
@@ -3,7 +3,34 @@ package CombinatorialProblems;
 import java.util.Scanner;
 
 public class _01_PermutationsWithoutRepetitions {
+    static String[] elements;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        elements = sc.nextLine().split("\\s+");
+
+        permute(0);
+    }
+
+    private static void permute(int index) {
+        if (index == elements.length) {
+            System.out.println(String.join(" ", elements));
+            return;
+        }
+
+        permute(index + 1);
+
+        for (int i = index + 1; i < elements.length; i++) {
+            swap(index, i);
+            permute(index + 1);
+            swap(index, i);
+        }
+    }
+
+    private static void swap(int index, int i) {
+        String temp = elements[index];
+        elements[index] = elements[i];
+        elements[i] = temp;
     }
 }
--- src/main/CombinatorialProblems/_02_PermutationsWithRepetitions.java
+++ src/main/CombinatorialProblems/_02_PermutationsWithRepetitions.java
@@ -3,7 +3,37 @@ package CombinatorialProblems;
 import java.util.Scanner;
 
 public class _02_PermutationsWithRepetitions {
+    static String[] elements;
+    static String[] currentElements;
+    static boolean[] isUsed;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        elements = sc.nextLine().split("\\s+");
+        currentElements = new String[elements.length];
+        isUsed = new boolean[elements.length];
+
+        permute(0);
+    }
+
+    private static void permute(int index) {
+        if (index == elements.length) {
+            printResult(currentElements);
+            return;
+        }
+
+        for (int i = 0; i < elements.length; i++) {
+            if (!isUsed[i]) {
+                isUsed[i] = true;
+                currentElements[index] = elements[i];
+                permute(index + 1);
+                isUsed[i] = false;
+            }
+        }
+    }
+
+    private static void printResult(String[] currentElements) {
+        System.out.println(String.join(" ", currentElements));
     }
 }
