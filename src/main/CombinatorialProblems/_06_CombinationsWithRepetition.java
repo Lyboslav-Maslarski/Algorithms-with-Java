48da080f407c6a45b32581d202cc8cc5df52034c
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-07-07 15:25:31 +0300

    add

--- src/main/CombinatorialProblems/_06_CombinationsWithRepetition.java
+++ src/main/CombinatorialProblems/_06_CombinationsWithRepetition.java
@@ -1,4 +1,30 @@
 package CombinatorialProblems;
 
+import java.util.Scanner;
+
 public class _06_CombinationsWithRepetition {
+    static String[] elements;
+    static String[] variations;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        elements = sc.nextLine().split("\\s+");
+        int n = sc.nextInt();
+        variations = new String[n];
+
+        combinations(0, 0);
+    }
+
+    private static void combinations(int index, int start) {
+        if (index == variations.length) {
+            System.out.println(String.join(" ", variations));
+            return;
+        }
+
+        for (int i = start; i < elements.length; i++) {
+            variations[index] = elements[i];
+            combinations(index + 1, i);
+        }
+    }
 }
--- src/main/CombinatorialProblems/_07_N_Choose_K_Count.java
+++ src/main/CombinatorialProblems/_07_N_Choose_K_Count.java
@@ -1,4 +1,23 @@
 package CombinatorialProblems;
 
+import java.util.Scanner;
+
 public class _07_N_Choose_K_Count {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+
+        System.out.println(binom(n, k));
+    }
+
+    private static int binom(int n, int k) {
+        if (k > n) {
+            return 0;
+        }
+        if (k == 0 || k == n) {
+            return 1;
+        }
+        return binom(n - 1, k - 1) + binom(n - 1, k);
+    }
 }
