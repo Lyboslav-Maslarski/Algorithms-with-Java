91c4d96ad56b8d495bf074a8a554ff812dc2b998
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-07-13 17:28:01 +0300

    add

--- src/main/DynamicProgrammingExercise/_04_SumWithUnlimitedCoins.java
+++ src/main/DynamicProgrammingExercise/_04_SumWithUnlimitedCoins.java
@@ -1,4 +1,24 @@
 package DynamicProgrammingExercise;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class _04_SumWithUnlimitedCoins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] coins = Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        int targetSum = Integer.parseInt(sc.nextLine());
+
+        int[] table = new int[targetSum + 1];
+        table[0] = 1;
+
+        for (int coin : coins) {
+            for (int j = coin; j <= targetSum; j++) {
+                table[j] += table[j - coin];
+            }
+        }
+        System.out.println(table[targetSum]);
+    }
 }
--- src/main/DynamicProgrammingExercise/_05_SumWithLimitedCoins.java
+++ src/main/DynamicProgrammingExercise/_05_SumWithLimitedCoins.java
@@ -1,4 +1,31 @@
 package DynamicProgrammingExercise;
 
+import java.util.*;
+
 public class _05_SumWithLimitedCoins {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] coins = Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        int targetSum = Integer.parseInt(sc.nextLine());
+
+        Map<Integer, Integer> dp = new HashMap<>();
+
+        int result = 0;
+        dp.put(0, 0);
+
+        for (int currentCoin : coins) {
+
+            for (Integer number : new ArrayList<>(dp.keySet())) {
+                int newSum = currentCoin + number;
+
+                if (newSum == targetSum) {
+                    result++;
+                }
+                dp.putIfAbsent(newSum, number);
+            }
+        }
+        System.out.println(result);
+    }
 }
