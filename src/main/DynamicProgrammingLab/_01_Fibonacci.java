eea2e6058b8f4a8107cf09abfe57bbfc3ffeab84
Lyboslav-Maslarski <lyboslav.maslarski@gmail.com>
2023-07-13 10:59:40 +0300

    add

--- src/main/DynamicProgrammingLab/_01_Fibonacci.java
+++ src/main/DynamicProgrammingLab/_01_Fibonacci.java
@@ -1,4 +1,26 @@
 package DynamicProgrammingLab;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class _01_Fibonacci {
+    static Map<Integer, Long> memory = new HashMap<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(fibonacci(n));
+    }
+
+    private static long fibonacci(int n) {
+        if (n <= 2) {
+            return 1;
+        }
+        if (memory.containsKey(n)) {
+            return memory.get(n);
+        }
+        memory.put(n, fibonacci(n - 1) + fibonacci(n - 2));
+        return memory.get(n);
+    }
 }
--- src/main/DynamicProgrammingLab/_02_LongestIncreasingSubsequence.java
+++ src/main/DynamicProgrammingLab/_02_LongestIncreasingSubsequence.java
@@ -1,4 +1,49 @@
 package DynamicProgrammingLab;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class _02_LongestIncreasingSubsequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] sequence = Arrays.stream(sc.nextLine().split("\\s+")).mapToInt(Integer::parseInt).toArray();
+
+        int[] length = new int[sequence.length];
+        int[] prev = new int[sequence.length];
+        Arrays.fill(prev, -1);
+
+        int maxLength = 0, maxIndex = -1;
+
+        for (int i = 0; i < sequence.length; i++) {
+            int current = sequence[i];
+            int bestLength = 1;
+            int bestIndex = -1;
+
+            for (int j = 0; j < i; j++) {
+                if (sequence[j] < current && length[j] + 1 > bestLength) {
+                    bestLength = length[j] + 1;
+                    bestIndex = j;
+                }
+            }
+
+            prev[i] = bestIndex;
+            length[i] = bestLength;
+
+            if (maxLength < bestLength) {
+                maxLength = bestLength;
+                maxIndex = i;
+            }
+        }
+
+        List<Integer> LIS = new ArrayList<>();
+
+        while (maxIndex != -1) {
+            LIS.add(sequence[maxIndex]);
+            maxIndex = prev[maxIndex];
+        }
+
+        Collections.reverse(LIS);
+        System.out.println(LIS.stream().map(String::valueOf).collect(Collectors.joining(" ")));
+    }
 }
